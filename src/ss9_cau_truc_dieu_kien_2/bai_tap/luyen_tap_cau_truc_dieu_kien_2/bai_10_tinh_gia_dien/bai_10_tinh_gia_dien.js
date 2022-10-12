7b509b640eb28ce7063e4ead0dfb43620175cf35
unknown <you@example.com>
2022-10-12 19:49:12 +0700

    ss10

--- src/ss9_cau_truc_dieu_kien_2/bai_tap/luyen_tap_cau_truc_dieu_kien_2/bai_10_tinh_gia_dien/bai_10_tinh_gia_dien.js
+++ src/ss9_cau_truc_dieu_kien_2/bai_tap/luyen_tap_cau_truc_dieu_kien_2/bai_10_tinh_gia_dien/bai_10_tinh_gia_dien.js
@@ -1,15 +1,15 @@
-let sodien = parseFloat(prompt("Nhập số điện tiêu thụ: "))
-if( sodien > 400){
+let soDien = parseFloat(prompt("Nhập số điện tiêu thụ: "))
+if( soDien > 400){
     alert("Số tiền cần phải đóng là: "+ (a*2927) +" VNĐ")}
-    else if( sodien > 300){
+    else if( soDien > 300){
     alert("Số tiền cần phải đóng là: "+ (a*2834) +" VNĐ")}
-    else if( sodien > 200){
+    else if( soDien > 200){
     alert("Số tiền cần phải đóng là: "+ (a*2536) +" VNĐ")}
-    else if( sodien > 100){
+    else if( soDien > 100){
     alert("Số tiền cần phải đóng là: "+ (a*2014) +" VNĐ")}
-    else if( sodien > 50){
+    else if( soDien > 50){
     alert("Số tiền cần phải đóng là: "+ (a*1734) +" VNĐ")}
-    else if( sodien > 0){
+    else if( soDien > 0){
     alert("Số tiền cần phải đóng là: "+ (a*1678) +" VNĐ")}
     else
         alert("Số tiêu thụ điện nhập không đúng.")
